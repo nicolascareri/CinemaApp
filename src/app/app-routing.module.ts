552b7e5acbdb8f6302540c11cf3bd0a63bb842df
Nicolás Careri <nicolascareri@gmail.com>
2019-10-26 19:53:53 -0300

    Modificado botón y agregado login frame

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BodyComponent } from './body/body.component';
+import { LoginComponent } from './login/login.component';
 
 
 export const routes: Routes = [
-  {path: '', component: BodyComponent},
+  {path: '', component: LoginComponent},
   {path: 'inicio', component: BodyComponent},
   {path: '**', component: BodyComponent}
 ];
@@ -13,4 +14,5 @@ export const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
