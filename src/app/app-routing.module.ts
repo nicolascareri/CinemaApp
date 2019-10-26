9379a718b756e86ae143b23f16296b9c116d3c39
Nicolás Careri <nicolascareri@gmail.com>
2019-10-26 20:34:16 -0300

    Cambios footer y navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
   {path: '', component: LoginComponent},
+  {path: 'login', component: LoginComponent},
   {path: 'inicio', component: BodyComponent},
   {path: '**', component: BodyComponent}
 ];
