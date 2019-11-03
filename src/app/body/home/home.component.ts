6d0c36d518640d2992dbcdf881d59ae26f2cf984
Alan Diaz <alan.yd.ph@gmail.com>
2019-11-03 15:06:38 -0300

    primer paso del carousel

--- src/app/body/home/home.component.ts
+++ src/app/body/home/home.component.ts
@@ -8,7 +8,33 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor() { }
+  carruseles =[
+    {
+      "titulo": "Nicolas, ¿Te gustaria continuar alguna de estas?",
+      "urls" :[
+        [
 
+          "assets/films/dark.jpg",
+          "assets/films/dark.jpg",
+          "assets/films/dark.jpg",
+          "assets/films/mindhunter.jpg",
+        ],[
+          
+          "assets/films/vikings.jpg",
+          "assets/films/Downton.jpg",
+          "assets/films/Downton.jpg",
+          "assets/films/Downton.jpg",
+        ],[
+          "assets/films/mindhunter.jpg",
+          "assets/films/vikings.jpg",
+
+          "assets/films/mindhunter.jpg",
+          "assets/films/vikings.jpg"
+        ]
+      ],
+      seguirviendo:true
+    }
+  ]
   ngOnInit() {
   }
 
--- src/app/carrusel/carrusel.component.ts
+++ src/app/carrusel/carrusel.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import * as $ from 'jquery';
+import { Component, OnInit,  Input  } from '@angular/core';
 @Component({
   selector: 'app-carrusel',
   templateUrl: './carrusel.component.html',
@@ -7,46 +6,8 @@ import * as $ from 'jquery';
 })
 export class CarruselComponent implements OnInit {
   constructor() { }
-  urls=[
-    "assets/films/dark.jpg",
-    "assets/films/vikings.jpg",
-    "assets/films/Downton.jpg",
-    "assets/films/mindhunter.jpg",
-    // "assets/films/peaky.jpg",
-    // "assets/films/vikings.jpg",
-    // "assets/films/Downton.jpg"
-]
-  carruseles =[
-    {
-      "titulo": "Nicolas, ¿Te gustaria continuar alguna de estas?",
-      "urls" :[
-          "assets/films/dark.jpg",
-          "assets/films/vikings.jpg",
-          "assets/films/Downton.jpg",
-          "assets/films/mindhunter.jpg",
-          // "assets/films/peaky.jpg",
-          // "assets/films/vikings.jpg",
-          // "assets/films/Downton.jpg"
-      ],
-      seguirviendo:true
-    },
-    {
-      "titulo": "O, ¿Te gustaria comenzar algo de tu lista?",
-      "urls" :[
-        "assets/films/carta.jpg",
-        "assets/films/descarga (4).jpg",
-        "assets/films/dark.jpg",
-        "assets/films/mindhunter.jpg",
-        "assets/films/vikings.jpg",
-        "assets/films/Downton.jpg",
-          "assets/films/vikings.jpg"
-      ],
-      seguirviendo:false
-    }
-  ]
+  @Input() carrusel: {};
 
-  public ngOnInit()
-  {
-   
+  public ngOnInit(){
   }
 }
