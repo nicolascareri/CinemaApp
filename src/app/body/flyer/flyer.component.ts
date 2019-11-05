468628ce845b3ff40759e5038d2f9e7f45fd1c7f
jcanneva <jcanneva@alumnos.exa.unicen.edu.ar>
2019-11-04 21:54:42 -0300

    Acomodado flyer para info

--- src/app/body/flyer/flyer.component.ts
+++ src/app/body/flyer/flyer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-flyer',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./flyer.component.css']
 })
 export class FlyerComponent implements OnInit {
-
+  @Input() info;
   constructor() { }
 
   ngOnInit() {
