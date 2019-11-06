1e41f801ea12ad45530d6bf2d9851135aa6b47bc
Alan Diaz <alan.yd.ph@gmail.com>
2019-11-05 21:57:19 -0300

    toqueteo vario

--- src/app/body/flyer/flyer.component.ts
+++ src/app/body/flyer/flyer.component.ts
@@ -7,14 +7,51 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class FlyerComponent implements OnInit {
   @Input() info;
-  constructor() { }
-
+  
+  constructor() {
+   
+   }
+   carruseles =[
+    {
+      "titulo": "Nicolas, ¿Te gustaria continuar alguna de estas?",
+      "urls" :[
+        [
+          "assets/films/dark.jpg",
+          "assets/films/vikings.jpg",
+          "assets/films/mindhunter.jpg",
+          "assets/films/Downton.jpg",
+        ],[
+          "assets/films/avengers.png",
+          "assets/films/aladin.jpg",
+          "assets/films/alita.jpg",
+          "assets/films/skyfall.jpg",
+        ],[
+          "assets/films/war.jpg",
+          "assets/films/toystory.jpg",
+          "assets/films/joker.jpg",
+          "assets/films/mindhunter.jpg",
+        ]
+      ],
+      seguirviendo:true
+    }
+  ]
   toggleVideo(){
     let video = document.getElementById("video") as HTMLElement;
-    video.style.height = "100%";
+    video.classList.add("pelicula");
+    video.classList.remove("d-none");
+    
+    let ventana = document.getElementById("ventana") as HTMLElement;
+    ventana.classList.remove("d-none");
+    let cerrar = document.getElementById("cerrar");
+    cerrar.classList.remove("d-none");
+    cerrar.classList.add("d-flex", "justify-self-end", "align-self-start", "boton");
+    cerrar.addEventListener("click", function(){
+  
+    });
+
   }
   public ngOnInit(){
-    
+
   }
 
 }
