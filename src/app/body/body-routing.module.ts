56c551c8bf952c800e2ce0bf0fe88a61c72577c9
alan <alanyd.ph@gmail.com>
2019-11-08 01:09:06 -0300

    agregado y pseudo finalizado de cuenta

--- src/app/body/body-routing.module.ts
+++ src/app/body/body-routing.module.ts
@@ -4,12 +4,17 @@ import { HomeComponent } from './home/home.component';
 import { PeliculasComponent } from './peliculas/peliculas.component';
 import { BodyComponent } from './body.component';
 import { InfoComponent } from './info/info.component';
+import { MicuentaComponent } from '../micuenta/micuenta.component';
 
 
 const routes: Routes = [
   { path: 'inicio', component: BodyComponent, children: [
     {path: '', component: HomeComponent},
     {path: 'peliculas', component: PeliculasComponent},
+    {path: 'cuenta', redirectTo: '/cuenta',
+    pathMatch: 'full'},
+    {path: 'login', redirectTo: '/login',
+    pathMatch: 'full'},
     {path: 'info', component: InfoComponent},
   ]}
 ];
--- src/app/body/body.module.ts
+++ src/app/body/body.module.ts
@@ -10,24 +10,28 @@ import { CarruselComponent } from '../carrusel/carrusel.component';
 import { CardComponent } from '../card/card.component';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { InfoComponent } from './info/info.component';
+import { MicuentaModule } from '../micuenta/micuenta.module';
+import { MicuentaRoutingModule } from '../micuenta/micuenta-routing.module';
 
 
 @NgModule({
   declarations: [
     FlyerComponent,
-    HomeComponent, 
+    HomeComponent,
     PeliculasComponent,
     CarruselComponent,
-    CardComponent, 
-    NavComponent, 
-    InfoComponent
+    CardComponent,
+    NavComponent,
+    InfoComponent,
   ],
   imports: [
+    MicuentaRoutingModule,
+    MicuentaModule,
     CommonModule,
     BodyRoutingModule,
     CarouselModule.forRoot(),
   ],
-  exports:[
+  exports: [
     NavComponent
   ]
 })
